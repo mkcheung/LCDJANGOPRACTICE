f95d723f366bf802df2c2ceb453af8097d675771
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-04 14:05:42 -0700

    establish problem model

--- problems/models.py
+++ problems/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Problem(models.Model):
+    class Meta:
+        db_table = 'problems'
+
+    # Define Enums for difficulty
+    class Difficulty(models.TextChoices):
+        EASY = "EASY", "Easy"
+        MEDIUM = "MEDIUM", "Medium"
+        HARD = "HARD", "Hard"
+
+    slug = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=100, unique=True)
+    leetcode_number = models.IntegerField(null=True, unique=True)
+    difficulty = models.CharField(max_length=6, choices=Difficulty.choices)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
--- problems/views.py
+++ problems/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from models import Problem
 
 from .serializers import TwoSumSerializer, ThreeSumSerializer, LongestNonRepeatingSubstringSerializer
 
@@ -24,6 +25,7 @@ class TwoSumView(APIView):
     def post(self, request):
         serializer = TwoSumSerializer(data=request.data)
         if serializer.is_valid():
+            problem = Problem.objects.get_or_create(slug="two_sum", title="TWO SUM", leetcode_number=1, difficulty="EASY")
             result = serializer.save()
 
             return Response(
